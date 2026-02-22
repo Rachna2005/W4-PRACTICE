a7c5702860bbf7225b788d40a4d3186db05d114f
rachna <rachnaoem07@gmail.com>
2026-02-22 08:42:14 +0700

    TASK-001 – The Color App

---
+++ lib/w4/lib/1_color_app/color_service.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ColorService extends ChangeNotifier {
+  int redTapCount = 0;
+  int blueTapCount = 0;
+
+  void incrementRedTapCount() {
+    redTapCount++;
+    notifyListeners();
+  }
+
+  void incrementBlueTapCount() {
+    blueTapCount++;
+    notifyListeners();
+  }
+}
